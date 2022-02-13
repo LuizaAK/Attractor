e60e2dda438f3f137d9eec668d9a88bc44e145d1
LuizaAK <81632625+LuizaAK@users.noreply.github.com>
2022-02-13 11:23:02 +0600

    Update dropdown.page.js

--- dropdown.page.js
+++ dropdown.page.js
@@ -1,14 +1,14 @@
-const DropdownPage = require('../pageobjects/Dropdown.page'); 
+const DropdownPage = require('../pageobjects/Dropdown.page'); //наследование страницы, url зашит в wdio.conf.js
 const elem1 = $('#Option1');
 const elem2 = $('#Option2');
 
 describe('Dropdown page'), () => {
     it('should select options', async () => {     //группирую тест в mocha, далее использую assertions 
-        await DropdownPage.open();
+        await DropdownPage.open();                 //открывает нужную страницу
+        await DropdownPage.clickDropdown.click();   //кликает на dropdown стрелку
+        await expect(elem1).toBeSelected().true;    //выбирает option 1
         await DropdownPage.clickDropdown.click();
-        await expect(elem1).toBeSelected().true; 
-        await DropdownPage.clickDropdown.click();
-        await expect(elem2).toBeSelected().true; 
+        await expect(elem2).toBeSelected().true;    //выбирает option 2
            
         
         
