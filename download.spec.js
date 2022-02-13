c1cc7b6fac12168916d21c2073abb61f919c5a68
LuizaAK <81632625+LuizaAK@users.noreply.github.com>
2022-02-14 01:26:44 +0600

    Update download.spec.js

--- download.spec.js
+++ download.spec.js
@@ -1,6 +1,6 @@
 const DownloadPage = require('../pageobjects/Download.page'); //importing Page from pageobject
 
-    describe('Download page', function () {                   //"describe\it" mocha function
+    describe('1 FD Download page', function () {                   //"describe\it" mocha function
         it('should download "some-file.txt" file', async () => {  //description of what this function should do
             await DownloadPage.open();
             await expect(href="download/some-file.txt").toHaveLink('download/some-file.txt').true; //supposed a file to be downloaded
