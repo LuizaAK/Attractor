863a73c82ac827a58d1240253dfa070d9c9063d5
LuizaAK <81632625+LuizaAK@users.noreply.github.com>
2022-02-13 11:18:09 +0600

    Update dropdown.page.js

--- dropdown.page.js
+++ dropdown.page.js
@@ -1,12 +1,13 @@
-const DropdownPage = require('../pageobjects/Dropdown.page');
+const DropdownPage = require('../pageobjects/Dropdown.page'); 
 const elem1 = $('#Option1');
 const elem2 = $('#Option2');
 
 describe('Dropdown page'), () => {
-    it('should select options', async () => {
+    it('should select options', async () => {     //группирую тест в mocha, далее использую assertions 
         await DropdownPage.open();
         await DropdownPage.clickDropdown.click();
         await expect(elem1).toBeSelected().true; 
+        await DropdownPage.clickDropdown.click();
         await expect(elem2).toBeSelected().true; 
            
         
