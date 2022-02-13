ba9e54f297fa2448b3657728736258286435ad2c
LuizaAK <81632625+LuizaAK@users.noreply.github.com>
2022-02-13 11:39:26 +0600

    Create login.spec.js

---
+++ login.spec.js
@@ -0,0 +1,11 @@
+const LoginPage = require('../pageobjects/Login.page'); //url is in wdio.conf.js file
+
+
+describe('My Login page', () => {                              //async function using mocha
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!'); //allowed hardcoding to myself  due to lack of time
+       
+    });
+});
