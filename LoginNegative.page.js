a785a2e95022bedcef7beb56f5510a4004a06f6d
LuizaAK <81632625+LuizaAK@users.noreply.github.com>
2022-02-13 11:05:47 +0600

    Update LoginNegative.page.js

--- LoginNegative.page.js
+++ LoginNegative.page.js
@@ -1,13 +1,13 @@
-const {clearInputValue} = require('../../methods/helper')
+const {clearInputValue} = require('../../methods/helper') // наследование для второго it (не добавила здесь), чтобы стереть неверный username 
 
 describe('LOGIN PAGE', fn: () => {  
-     it('Should not login with any name except tomsmith', fn: async() => { 
-        await LoginPage.open();
-        await LoginPage.InputUsername.setValue('johndoe');
+     it('Should not login with any name except tomsmith', fn: async() => { // вводит неверный юзернейм, верный пароль, кликает, получает alert message
+        await LoginPage.open();                                             
+        await LoginPage.InputUsername.setValue('johndoe');                  
         await LoginPage.InputPassword.setValue('SuperSecretPassword!');
         await LoginPage.btnSubmit.click();
         
  const res = await.LoginPage.alertMsg.getText();
  await expect(res).toEqual( "Your username is invalid!");       
         
-    }
+    });
